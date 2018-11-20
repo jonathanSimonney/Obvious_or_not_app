abfd782838f48cf7ada51f9e9a27ae8c1b78607f
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-11-20 14:55:33 +0100

    Initial Commit

---
+++ Obvious_or_not/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Obvious_or_not
+//
+//  Created by Jonathan SIMONNEY on 20/11/2018.
+//  Copyright © 2018 Jonathan SIMONNEY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
