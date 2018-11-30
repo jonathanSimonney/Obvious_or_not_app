eaf1ff792abe8d80056bd56ba8c07e39b2aafa1e
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-11-30 16:54:18 +0100

    added the basic view for poll.

--- Obvious_or_not/PollController.swift
+++ Obvious_or_not/PollController.swift
@@ -15,11 +15,39 @@ class PollController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        
+        let unwrappedPoll = self.poll ?? Poll(id: "default id", title: "this poll doesn't exist", content: "false content, please report this as a bug", explanation: "something wrong happened", choices: [], totalVotes: 0, hasVoted: false)
 
         // Do any additional setup after loading the view.
         let titleView = UILabel()
-        titleView.text = self.poll?.title
-        self.view.addSubviewGrid(titleView, grid: [0, 0, 1, 1], collNumber: 1, rowNumber: 4)
+        titleView.text = unwrappedPoll.title
+        
+        let contentView = UILabel()
+        contentView.text = unwrappedPoll.content
+        contentView.numberOfLines = 0
+
+        let explanationView = UILabel()
+        explanationView.text = unwrappedPoll.explanation
+        explanationView.numberOfLines = 0
+        
+        let totalVotesView = UILabel()
+        totalVotesView.text =  String(unwrappedPoll.totalVotes) + " votes"
+        
+        let choiceContainer = UIView()
+        let numberChoice = unwrappedPoll.choices.count
+        
+        self.view.addSubviewGrid(titleView, grid: [0, 4, 1, 5], collNumber: 1, rowNumber: 40)
+        self.view.addSubviewGrid(contentView, grid: [0, 7, 1, 10], collNumber: 1, rowNumber: 40)
+        self.view.addSubviewGrid(explanationView, grid: [0, 11, 1, 14], collNumber: 1, rowNumber: 40)
+        self.view.addSubviewGrid(choiceContainer, grid: [0, 15, 1, 16 + numberChoice], collNumber: 1, rowNumber: 40)
+        self.view.addSubviewGrid(totalVotesView, grid: [0, 17 + numberChoice, 1, 18 + numberChoice], collNumber: 1, rowNumber: 40)
+        
+        for (index, choice) in unwrappedPoll.choices.enumerated(){
+            let choiceView = UILabel()
+            choiceView.text = choice.description + String(choice.percentage)
+            
+            choiceContainer.addSubviewGrid(choiceView, grid: [0, index, 1, index + 1], collNumber: 1, rowNumber: numberChoice)
+        }
     }
     
     func setPoll(poll: Poll){
