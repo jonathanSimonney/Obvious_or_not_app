5793a33f64d0fdc345fc45de2ed95cbf8124709b
SUP'Internet 05 <supinternet05@MacBook-Air-de-SUPInternet-7.local>
2018-12-13 18:04:42 +0100

    changed some stuff

--- Obvious_or_not/loggedOut/RegisterController.swift
+++ Obvious_or_not/loggedOut/RegisterController.swift
@@ -15,6 +15,8 @@ class RegisterController: UIViewController, UITextFieldDelegate {
     var usernameSignUpField: UITextField?
     var passwordSignUpField: UITextField?
     var verifSignUpField: UITextField?
+    var usernameSignInField: UITextField?
+    var passwordSignInField: UITextField?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,13 +52,12 @@ class RegisterController: UIViewController, UITextFieldDelegate {
         verifSignUpField?.returnKeyType = UIReturnKeyType.continue
         verifSignUpField?.frame = CGRect(x: 24, y: inscriptionButton.frame.minY - 60, width: fieldWidth, height: 36)
         verifSignUpField?.layer.cornerRadius = 10
-        verifSignUpField?.attributedPlaceholder = NSAttributedString(string: "password verification",
-                                                              attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        verifSignUpField?.attributedPlaceholder = NSAttributedString(string: "password verification")
         verifSignUpField?.backgroundColor = UIColor(white: 1, alpha: 0.5)
         
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 36))
         verifSignUpField?.leftView = paddingView
-        verifSignUpField?.leftViewMode = UITextField.ViewMode.always
+        //verifSignUpField?.leftViewMode = UITextField.leftViewMode.always
         verifSignUpField?.tag = 1
         verifSignUpField?.keyboardAppearance = .dark
         verifSignUpField?.delegate = self
@@ -66,13 +67,12 @@ class RegisterController: UIViewController, UITextFieldDelegate {
         passwordSignUpField?.returnKeyType = UIReturnKeyType.continue
         passwordSignUpField?.frame = CGRect(x: 24, y: (verifSignUpField?.frame.minY)! - 60, width: fieldWidth, height: 36)
         passwordSignUpField?.layer.cornerRadius = 10
-        passwordSignUpField?.attributedPlaceholder = NSAttributedString(string: "password",
-                                                                 attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        passwordSignUpField?.attributedPlaceholder = NSAttributedString(string: "password")
         passwordSignUpField?.backgroundColor = UIColor(white: 1, alpha: 0.5)
         
         let secondPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 36))
         passwordSignUpField?.leftView = secondPaddingView
-        passwordSignUpField?.leftViewMode = UITextField.ViewMode.always
+        //passwordSignUpField?.leftViewMode = UITextField.ViewMode.always
         passwordSignUpField?.tag = 2
         passwordSignUpField?.keyboardAppearance = .dark
         passwordSignUpField?.delegate = self
@@ -82,16 +82,52 @@ class RegisterController: UIViewController, UITextFieldDelegate {
         usernameSignUpField?.returnKeyType = UIReturnKeyType.continue
         usernameSignUpField?.frame = CGRect(x: 24, y: (passwordSignUpField?.frame.minY)! - 60, width: fieldWidth, height: 36)
         usernameSignUpField?.layer.cornerRadius = 10
-        usernameSignUpField?.attributedPlaceholder = NSAttributedString(string: "Username",
-                                                                  attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        usernameSignUpField?.attributedPlaceholder = NSAttributedString(string: "Username")
         usernameSignUpField?.backgroundColor = UIColor(white: 1, alpha: 0.5)
         
         let usernamePaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 36))
         usernameSignUpField?.leftView = usernamePaddingView
-        usernameSignUpField?.leftViewMode = UITextField.ViewMode.always
+        //usernameSignUpField?.leftViewMode = UITextField.ViewMode.always
         usernameSignUpField?.tag = 5
         usernameSignUpField?.keyboardAppearance = .dark
         usernameSignUpField?.delegate = self
+        
+        let loginButton = UIButton()
+        loginButton.frame = CGRect(x: 24, y: (usernameSignUpField?.frame.minY)! - 120, width: fieldWidth, height: 36)
+        loginButton.setTitle("Sign in", for: .normal)
+        loginButton.titleLabel?.textColor = UIColor.white
+        loginButton.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        loginButton.titleLabel?.textAlignment = .center
+        loginButton.layer.cornerRadius = 10
+        
+        self.passwordSignInField = UITextField()
+        passwordSignInField?.returnKeyType = UIReturnKeyType.continue
+        passwordSignInField?.frame = CGRect(x: 24, y: (loginButton.frame.minY) - 60, width: fieldWidth, height: 36)
+        passwordSignInField?.layer.cornerRadius = 10
+        passwordSignInField?.attributedPlaceholder = NSAttributedString(string: "password")
+        passwordSignInField?.backgroundColor = UIColor(white: 1, alpha: 0.5)
+        
+        let secondPaddingViewLogin = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 36))
+        passwordSignInField?.leftView = secondPaddingViewLogin
+        //passwordSignUpField?.leftViewMode = UITextField.ViewMode.always
+        passwordSignInField?.tag = 2
+        passwordSignInField?.keyboardAppearance = .dark
+        passwordSignInField?.delegate = self
+        passwordSignInField?.isSecureTextEntry = true
+        
+        self.usernameSignInField = UITextField()
+        usernameSignInField?.returnKeyType = UIReturnKeyType.continue
+        usernameSignInField?.frame = CGRect(x: 24, y: (passwordSignInField?.frame.minY)! - 60, width: fieldWidth, height: 36)
+        usernameSignInField?.layer.cornerRadius = 10
+        usernameSignInField?.attributedPlaceholder = NSAttributedString(string: "Username")
+        usernameSignInField?.backgroundColor = UIColor(white: 1, alpha: 0.5)
+        
+        let usernamePaddingViewLogin = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 36))
+        usernameSignInField?.leftView = usernamePaddingViewLogin
+        //usernameSignUpField?.leftViewMode = UITextField.ViewMode.always
+        usernameSignInField?.tag = 5
+        usernameSignInField?.keyboardAppearance = .dark
+        usernameSignInField?.delegate = self
         
         let imgBg = UIImageView()
         imgBg.image = UIImage(named: "background")
@@ -103,8 +139,12 @@ class RegisterController: UIViewController, UITextFieldDelegate {
         self.view.addSubview(verifSignUpField!)
         self.view.addSubview(passwordSignUpField!)
         self.view.addSubview(usernameSignUpField!)
+        self.view.addSubview(loginButton)
+        self.view.addSubview(passwordSignInField!)
+        self.view.addSubview(usernameSignInField!)
         
         inscriptionButton.addTarget(self, action: #selector(register), for: .touchDown)
+        loginButton.addTarget(self, action: #selector(login), for: .touchDown)
     }
     
     
@@ -124,6 +164,30 @@ class RegisterController: UIViewController, UITextFieldDelegate {
     @objc func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
+    }
+    
+    @objc private func login(){
+        var errors = [String]()
+        
+        if self.passwordSignInField?.text == ""{
+            errors.append("The password field is required")
+        }
+        
+        if self.usernameSignInField?.text == ""{
+            errors.append("The username field is required")
+        }
+    
+        
+        if errors.count == 0{
+            let parameters: Parameters = [
+                "username": self.usernameSignInField?.text ?? "",
+                "password": self.passwordSignInField?.text ?? "",
+                ]
+            
+            self.loginHandler(loginParams: parameters)
+        }else{
+            self.showErrors(errors: errors)
+        }
     }
     
     @objc private func register(){
@@ -173,10 +237,17 @@ class RegisterController: UIViewController, UITextFieldDelegate {
             in
             let jsonLoginResponse = JSON(loginResponse.result.value as Any)
             
-            let cache = UserDefaults.standard
-            cache.set(jsonLoginResponse["token"].stringValue, forKey: "userToken")
+            if jsonLoginResponse["status"] == 200{
+                let cache = UserDefaults.standard
+                cache.set(jsonLoginResponse["token"].stringValue, forKey: "userToken")
+                
+                self.showRightStoryboard()
+            }else{
+                print(jsonLoginResponse)
+                self.showErrors(errors: [jsonLoginResponse["error"].stringValue])
+            }
             
-            self.showRightStoryboard()
+
         }
     }
     
